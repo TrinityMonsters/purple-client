6d12fa25f6478111a9cb5b78b3fdc872aa0f260f
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2025-07-09 05:42:11 +0400

    Add ActiveSupport for delegate

--- lib/purple/responses/object.rb
+++ lib/purple/responses/object.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 # Purple returns objects as responses from API. This is a base class for all responses objects.
+require 'active_support/core_ext/module/delegation'
 class Purple::Responses::Object
   attr_accessor :attributes
 
--- purple-client.gemspec
+++ purple-client.gemspec
@@ -32,4 +32,5 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency 'dry-initializer'
+  spec.add_dependency 'activesupport'
 end
