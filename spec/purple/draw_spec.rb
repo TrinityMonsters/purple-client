bb8b5bebbefc8337c68b89d2e1da311f3b22051e
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2026-02-18 19:26:22 +0400

    Fix draw spec to avoid executing leaf path request

--- spec/purple/draw_spec.rb
+++ spec/purple/draw_spec.rb
@@ -2,8 +2,12 @@
 
 RSpec.describe '.draw' do
   it 'loads and evaluates DSL paths relative to the caller file' do
-    expect(TimeMagic::Client.api.full_path).to eq(:api)
-    expect(TimeMagic::Client.api.v1.full_path).to eq('api/v1')
-    expect(TimeMagic::Client.api.v1.status.full_path).to eq('api/v1/status')
+    api_path = TimeMagic::Client.api
+    v1_path = api_path.v1
+    status_path = v1_path.children.find { |child| child.name == :status }
+
+    expect(api_path.full_path).to eq(:api)
+    expect(v1_path.full_path).to eq('api/v1')
+    expect(status_path.full_path).to eq('api/v1/status')
   end
 end
